2c22ea1dc563f1c1e8053101b3680fb693e2ec71
SmartyJem <31848174+SmartyJem@users.noreply.github.com>
2019-09-13 16:40:05 +1200

    Add files via upload

---
+++ Portable Spider Cam/FileHandlerObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PortableSpiderCam
+{
+    class FileHandlerObject
+    {
+        // 1. PROPERTIES. (ALLOWS THE CLASS TO RECOGNISE THESE PASSED VARIABLE NAMES.)
+        private List<ConfigObject> list_Of_Files = new List<ConfigObject>();
+
+        // 2. CONSTRUCTOR. (ASSIGNS THE VALUES TO ITSELF: THE OBJECT.)
+        public FileHandlerObject()
+        {
+            this.list_Of_Files = new List<ConfigObject>();
+        }
+
+        // 3. METHODS. (PROVIDE FUNCTIONS WHICH CAN BE USED THROUGHOUT THE MAIN CODE.)
+        public void addFile(string file_Name)
+        {
+
+            if (!File.Exists(file_Name))
+            {
+                using (var file = new StreamWriter(file_Name, true))
+                {
+                    file.WriteLine("0,0,0,0,0,0,0,0,0");
+                }
+                this.list_Of_Files.Add(new ConfigObject(file_Name, 0, 0, 0, 0, 0, 0, 0, 0, 0));
+            }
+            else
+            {
+                using (var file = new StreamReader(file_Name, true))
+                {
+                    foreach (string line in File.ReadAllLines(file_Name))
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+                this.list_Of_Files.Add(new ConfigObject(file_Name, 0, 0, 0, 0, 0, 0, 0, 0, 0));
+            }
+
+        }
+
+    }
+}
