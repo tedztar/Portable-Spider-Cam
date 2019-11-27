3f4909a9eb10bf8adc0cf5da6cb69c70a4cc1988
tedztar <tedztar@gmail.com>
2019-11-27 20:14:59 +1300

    Added guis

--- Portable Spider Cam/ComputerRun.cs
+++ Portable Spider Cam/ComputerRun.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class Computer_run : Form
     {
-        public Form1()
+        public Computer_run()
         {
             InitializeComponent();
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
--- Portable Spider Cam/Run.cs
+++ Portable Spider Cam/Run.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class Run : Form
     {
-        public Form1()
+        public Run()
         {
             InitializeComponent();
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
